6022a6c7cf43ab769d3e2306b7092926f0d5c3df
Costache Florian-Alin <ccostache.alin@gmail.com>
2023-03-02 09:06:15 +0100

    Update adminLogin.py

--- src/admin_login_ui/adminLogin.py
+++ src/admin_login_ui/adminLogin.py
@@ -1,13 +1,16 @@
 import tkinter as tk
-from tkinter import ttk
 
 def login_admin_page():
     # create the main window
     window = tk.Tk()
     window.geometry("1200x900")
+<<<<<<< Updated upstream
     window.maxsize(width=1200,height=900)
     window.minsize(width=1200,height=900)
 
+=======
+    window.resizable(False,False)
+>>>>>>> Stashed changes
 
     # create two frames side by side
     frame1 = tk.Frame(window, width=600, height=900, bg="#D9D9D9")
@@ -17,8 +20,8 @@ def login_admin_page():
     frame2.pack(side="right")
 
     # add a label to frame1 with the specified properties
-    label = tk.Label(frame1, text="Admin Panel", font=("Inter", 24, "normal"), bg="#D9D9D9", fg="black", justify="left")
-    label.place(x=20, y=20, width=146, height=27)
+    label = tk.Label(frame1, text="Admin Panel", font=("Inter", 25, "normal"), bg="#D9D9D9", fg="black", justify="left")
+    label.place(x=20, y=20, width=190, height=50)
 
     # set the line height to 29 pixels and vertical alignment to top
     label.config(anchor="nw", pady=0, padx=0, wraplength=0, height=0, width=0)
@@ -36,19 +39,23 @@ def login_admin_page():
     pass_entry.place(x=153, y=350, width=200, height=28)
 
     # add a login button to frame1
-    login_button = tk.Button(frame1, text="Login", font=("Inter", 12), bg="#D9D9D9", fg="black",
+    login_button = tk.Button(frame1, text="Login", font=("Inter", 12), bg="white", fg="black",
                              bd=0, highlightthickness=0, activebackground="#B3B3B3",
                              command=lambda: print("Login button clicked"))
     login_button.place(x=200, y=450, width=82, height=24)
 
     # add a back button to frame1
-    login_button = tk.Button(frame1, text="Back", font=("Inter", 12), bg="#D9D9D9", fg="black",
+    login_button = tk.Button(frame1, text="Back", font=("Inter", 12), bg="white", fg="black",
                              bd=0, highlightthickness=0, activebackground="#B3B3B3",
                              command=lambda: print("Back button clicked"))
     login_button.place(x=20, y=700, width=82, height=24)
 
 
+<<<<<<< Updated upstream
     window.resizable(False,False)
+=======
+
+>>>>>>> Stashed changes
     # run the main loop
     window.mainloop()
 
