87eeb6eab492f2f083f9c9bfd12fd776f19c4f38
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2023-03-22 12:18:33 +0100

    Draft XML Schema

---
+++ src/XMLPae.py
@@ -0,0 +1,9 @@
+import xmlschema
+
+schema = xmlschema.XMLSchema('XMLSchema.xsd')
+
+try:
+    schema.validate('path/to/.xml')
+    print('The XML file is valid according to the schema.')
+except xmlschema.exceptions.XMLSchemaValidationError:
+    print('The XML file is not valid according to the schema.')
