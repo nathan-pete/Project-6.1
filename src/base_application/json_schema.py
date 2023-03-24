af67a2e4e54319a80d1d237dd8c899c112ade829
radoychev <62029581+radoychev@users.noreply.github.com>
2023-03-24 15:41:13 +0100

    Update json_schema.py

--- src/base_application/json_schema.py
+++ src/base_application/json_schema.py
@@ -1,42 +1,24 @@
-import json
 import jsonschema
+import json
 
+# Define the JSON schema
 schema = {
     "type": "object",
     "properties": {
-        "account_identification": {
-            "type": "string"
-        },
+        "account_identification": {"type": "string"},
         "available_balance": {
             "type": "object",
             "properties": {
                 "amount": {
                     "type": "object",
                     "properties": {
-                        "amount": {
-                            "type": "string"
-                        },
-                        "currency": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "amount",
-                        "currency"
-                    ]
+                        "amount": {"type": "string"},
+                        "currency": {"type": "string"}
+                    }
                 },
-                "date": {
-                    "type": "string"
-                },
-                "status": {
-                    "type": "string"
-                }
-            },
-            "required": [
-                "amount",
-                "date",
-                "status"
-            ]
+                "date": {"type": "string"},
+                "status": {"type": "string"}
+            }
         },
         "final_closing_balance": {
             "type": "object",
@@ -44,30 +26,13 @@ schema = {
                 "amount": {
                     "type": "object",
                     "properties": {
-                        "amount": {
-                            "type": "string"
-                        },
-                        "currency": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "amount",
-                        "currency"
-                    ]
+                        "amount": {"type": "string"},
+                        "currency": {"type": "string"}
+                    }
                 },
-                "date": {
-                    "type": "string"
-                },
-                "status": {
-                    "type": "string"
-                }
-            },
-            "required": [
-                "amount",
-                "date",
-                "status"
-            ]
+                "date": {"type": "string"},
+                "status": {"type": "string"}
+            }
         },
         "final_opening_balance": {
             "type": "object",
@@ -75,30 +40,13 @@ schema = {
                 "amount": {
                     "type": "object",
                     "properties": {
-                        "amount": {
-                            "type": "string"
-                        },
-                        "currency": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "amount",
-                        "currency"
-                    ]
+                        "amount": {"type": "string"},
+                        "currency": {"type": "string"}
+                    }
                 },
-                "date": {
-                    "type": "string"
-                },
-                "status": {
-                    "type": "string"
-                }
-            },
-            "required": [
-                "amount",
-                "date",
-                "status"
-            ]
+                "date": {"type": "string"},
+                "status": {"type": "string"}
+            }
         },
         "forward_available_balance": {
             "type": "object",
@@ -106,134 +54,48 @@ schema = {
                 "amount": {
                     "type": "object",
                     "properties": {
-                        "amount": {
-                            "type": "string"
-                        },
-                        "currency": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "amount",
-                        "currency"
-                    ]
+                        "amount": {"type": "string"},
+                        "currency": {"type": "string"}
+                    }
                 },
-                "date": {
-                    "type": "string"
-                },
-                "status": {
-                    "type": "string"
-                }
-            },
-            "required": [
-                "amount",
-                "date",
-                "status"
-            ]
+                "date": {"type": "string"},
+                "status": {"type": "string"}
+            }
         },
-        "sequence_number": {
-            "type": "null"
-        },
-        "statement_number": {
-            "type": "string"
-        },
-        "transaction_reference": {
-            "type": "string"
-        },
+        "sequence_number": {"type": ["string", "null"]},
+        "statement_number": {"type": "string"},
+        "transaction_reference": {"type": "string"},
         "transactions": {
             "type": "array",
-            "items": [
-                {
-                    "type": "object",
-                    "properties": {
-                        "amount": {
-                            "type": "object",
-                            "properties": {
-                                "amount": {
-                                    "type": "string"
-                                },
-                                "currency": {
-                                    "type": "string"
-                                }
-                            },
-                            "required": [
-                                "amount",
-                                "currency"
-                            ]
-                        },
-                        "bank_reference": {
-                            "type": "string"
-                        },
-                        "currency": {
-                            "type": "string"
-                        },
-                        "customer_reference": {
-                            "type": "string"
-                        },
-                        "date": {
-                            "type": "string"
-                        },
-                        "entry_date": {
-                            "type": "string"
-                        },
-                        "extra_details": {
-                            "type": "string"
-                        },
-                        "funds_code": {
-                            "type": "null"
-                        },
-                        "guessed_entry_date": {
-                            "type": "string"
-                        },
-                        "id": {
-                            "type": "string"
-                        },
-                        "status": {
-                            "type": "string"
-                        },
-                        "transaction_details": {
-                            "type": "string"
+            "items": {
+                "type": "object",
+                "properties": {
+                    "amount": {
+                        "type": "object",
+                        "properties": {
+                            "amount": {"type": "string"},
+                            "currency": {"type": "string"}
                         }
                     },
-                    "required": [
-                        "amount",
-                        "bank_reference",
-                        "currency",
-                        "customer_reference",
-                        "date",
-                        "entry_date",
-                        "extra_details",
-                        "funds_code",
-                        "guessed_entry_date",
-                        "id",
-                        "status",
-                        "transaction_details"
-                    ]
-                }
-            ]
+                    "bank_reference": {"type": "string"},
+                    "currency": {"type": "string"},
+                    "customer_reference": {"type": "string"},
+                    "date": {"type": "string"},
+                    "entry_date": {"type": "string"},
+                    "extra_details": {"type": "string"},
+                    "funds_code": {"type": ["string", "null"]},
+                    "guessed_entry_date": {"type": "string"},
+                    "id": {"type": "string"},
+                    "status": {"type": "string"},
+                    "transaction_details": {"type": "string"}
+                },
+                "required": ["amount", "bank_reference", "currency", "date", "entry_date", "id", "status", "transaction_details"]
+            }
         }
     },
-    "required": [
-        "account_identification",
-        "available_balance",
-        "final_closing_balance",
-        "final_opening_balance",
-        "forward_available_balance",
-        "sequence_number",
-        "statement_number",
-        "transaction_reference",
-        "transactions"
-    ]
-
+    "required": ["account_identification", "available_balance", "final_closing_balance", "final_opening_balance", "forward_available_balance", "statement_number", "transaction_reference", "transactions"]
 }
 
-# Load the MT940 file as a JSON object
-with open('C:/Users/Omen/IdeaProjects/pr1/venv/json_1.json.') as json_file:
-    mt940_json = json.load(json_file)
+with open('C:/Users/Omen/IdeaProjects/pr1/venv/json_1.json', 'r') as f:
+    mt940_data = json.load(f)
 
-try:
-    jsonschema.validate(mt940_json, schema)
-    print("MT940 file is valid.")
-except jsonschema.exceptions.ValidationError as e:
-    print("MT940 file is not valid. Validation error: ")
-    print(e)
