1f78f988e5dc0aeb11a816cc17799de280378fca
micriteo <micriteo@yahoo.com>
2023-04-04 11:17:46 +0200

    Select Multiple mt940 files

--- src/base_application/fileUpload.py
+++ src/base_application/fileUpload.py
@@ -10,46 +10,51 @@ class MainWindow:
         self.master.title("Sports Accounting - MT940 Parser")
         self.master.wm_attributes("-topmost", 1)
 
-        # Create the "Select File" button
-        self.select_file_button = Button(self.master, text="Select File", command=self.select_file)
-        self.select_file_button.pack()
+        # Create the "Select Files" button
+        self.select_files_button = Button(self.master, text="Select Files", command=self.select_files)
+        self.select_files_button.pack()
 
-        # Create the label to display the selected file name
-        self.selected_file_label = Label(self.master, text="")
-        self.selected_file_label.pack()
+        # Create the label to display the selected files
+        self.selected_files_label = Label(self.master, text="")
+        self.selected_files_label.pack()
 
         # Create the "Parse" button
-        self.parse_button = Button(self.master, text="Parse", command=self.parse_file)
+        self.parse_button = Button(self.master, text="Parse", command=self.parse_files)
         self.parse_button.pack()
 
-    def select_file(self):
-        """Open a file dialog to select an MT940 file."""
-        file_path = filedialog.askopenfilename(defaultextension=".sta", filetypes=[("MT940 Files", "*.sta"), ("All Files", "*.*")])
-        self.selected_file_label.config(text=file_path)
-        self.file_path = file_path
+        # Initialize list of file paths
+        self.file_paths = []
 
-    def parse_file(self):
-        """Parse the selected MT940 file and save the result as a JSON file."""
-        # Check MT940 file
-        if check_mt940_file(self.file_path):
-            # Save to NoSQL DB
-            url = api_server_ip + '/api/uploadFile'
-            payload = {'file_path': self.file_path}
-            response = requests.post(url, data=payload)
+    def select_files(self):
+        """Open a file dialog to select MT940 files."""
+        file_paths = filedialog.askopenfilenames(defaultextension=".sta", filetypes=[("MT940 Files", "*.sta"), ("All Files", "*.*")])
+        self.selected_files_label.config(text="Selected Files: " + str(file_paths))
+        self.file_paths = file_paths
 
-            # Save to SQL DB FILE
-            payload = get_json_payload_mt940_file(self.file_path)
-            reference = payload["referencenumber"]
-            url = api_server_ip + '/api/insertFile/' + str(payload["referencenumber"]) + "/" + str(payload["statementnumber"]) + "/" + str(payload["sequencedetail"]) + "/" + str(payload["availablebalance"]) + "/" + str(payload["forwardavbalance"]) + "/" + str(payload["accountid"])
-            response = requests.get(url)
-
-            # Save to SQL DB Transaction
-            json_trans = parse_mt940_file(self.file_path)
-            for trans in json_trans["transactions"]:
-                url = api_server_ip + '/api/insertTransaction'
-                payload = get_json_payload_transaction(trans)
-                payload.update(referencenumber = reference)
+    def parse_files(self):
+        """Parse the selected MT940 files and save the results as JSON files."""
+        for file_path in self.file_paths:
+            # Check MT940 file
+            if check_mt940_file(file_path):
+                # Save to NoSQL DB
+                url = api_server_ip + '/api/uploadFile'
+                payload = {'file_path': file_path}
                 response = requests.post(url, data=payload)
+
+                # Save to SQL DB FILE
+                payload = get_json_payload_mt940_file(file_path)
+                reference = payload["referencenumber"]
+                url = api_server_ip + '/api/insertFile/' + str(payload["referencenumber"]) + "/" + str(payload["statementnumber"]) + "/" + str(payload["sequencedetail"]) + "/" + str(payload["availablebalance"]) + "/" + str(payload["forwardavbalance"]) + "/" + str(payload["accountid"])
+                response = requests.get(url)
+
+                # Save to SQL DB Transaction
+                json_trans = parse_mt940_file(file_path)
+                for trans in json_trans["transactions"]:
+                    url = api_server_ip + '/api/insertTransaction'
+                    payload = get_json_payload_transaction(trans)
+                    payload.update(referencenumber = reference)
+                    response = requests.post(url, data=payload)
+
         # Close Window
         self.master.destroy()
 
@@ -61,4 +66,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
